b46559360b65ce9ee0d23e493d6c2eabce59b39a
Vincent <oa5136@gmail.com>
2025-01-07 15:36:10 +0800

    updated settings for Github pages' deployment.

--- quasar.conf.js
+++ quasar.conf.js
@@ -74,7 +74,9 @@ module.exports = configure(function (/* ctx */) {
       // polyfillModulePreload: true,
       // distDir
 
-      // extendViteConf (viteConf) {},
+      extendViteConf (viteConf) {
+        viteConf.base = './'
+      },
       // viteVuePluginOptions: {},
 
       vitePlugins: [
