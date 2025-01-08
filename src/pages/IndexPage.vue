bba0883808282b5b3db7bcef441563d5d17abb05
Vincent <oa5136@gmail.com>
2025-01-08 15:13:34 +0800

    CSS style adjustment for RWD

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
-    <Constructor></Constructor>
-    <Form></Form>
+  <q-page class="flex q-py-md flex-jc">
+    <div class="col-lg col-md col-sm-12 col-xs-12 q-mx-md">
+      <Constructor></Constructor>
+    </div>
+    <div class="col-lg col-md col-sm-12 col-xs-12 q-mx-md">
+      <Form></Form>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-import Constructor from './Constructor/index.vue'
-import Form from './Form/index.vue'
+import Constructor from 'components/Constructor/index.vue'
+import Form from 'components/Form/index.vue'
 
 defineOptions({
   name: 'IndexPage'
