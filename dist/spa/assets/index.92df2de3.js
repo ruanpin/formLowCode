e823ba1c37292ad6e22dbe23b3f46f3d895a4e95
Vincent <oa5136@gmail.com>
2025-01-16 15:40:02 +0800

    build: bundle frontend project for production.

---
+++ dist/spa/assets/index.92df2de3.js
@@ -0,0 +1,1 @@
+import{a5 as e,aq as t}from"./index.56107e59.js";const u=Object.assign({name:"MutiInputsColumnComponent"},{__name:"index",props:{renderObject:{type:Object,required:!0}},setup(n){return(o,r)=>(e(),t("div",null," MutiInputsColumnComponent "))}});export{u as default};
